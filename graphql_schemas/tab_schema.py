e997b8b2f9e19fbbf309c6b532e4ae68ade5dcaf
Simon Norman <01secn@gmail.com>
2019-01-30 16:36:06 +0000

    Api expanded to create tabs

--- graphql_schemas/tab_schema.py
+++ graphql_schemas/tab_schema.py
@@ -1,7 +1,27 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models.models import TabRecording as TabRecordingModel
+from models.models import TabRecording as TabRecordingModel, Tab as TabModel
 from models.models import db
+
+
+class TabObject(SQLAlchemyObjectType):
+    class Meta:
+        model = TabModel
+        interfaces = (graphene.relay.Node,)
+
+
+class CreateTab(graphene.Mutation):
+    class Arguments:
+        tab_id = graphene.Int(required=True)
+        timestamp_created = graphene.DateTime(required=True)
+
+    tab = graphene.Field(lambda: TabObject)
+
+    def mutate(self, info, tab_id, timestamp_created):
+        tab = TabModel(tab_id=tab_id, last_timestamp_active=timestamp_created)
+        db.session.add(tab)
+        db.session.commit()
+        return CreateTab(tab=tab)
 
 
 class TabRecordingObject(SQLAlchemyObjectType):
@@ -12,15 +32,13 @@ class TabRecordingObject(SQLAlchemyObjectType):
 
 class CreateTabRecording(graphene.Mutation):
     class Arguments:
-        tabid = graphene.String(required=True)
+        tab_id = graphene.String(required=True)
         status = graphene.String(required=True)
 
     tab_recording = graphene.Field(lambda: TabRecordingObject)
 
-    def mutate(self, info, tabid, status):
-
-        tab_recording = TabRecordingModel(tabid=tabid, status=status)
-
+    def mutate(self, info, tab_id, status, timestamp_recorded):
+        tab_recording = TabRecordingModel(tab_id=tab_id, status=status)
         db.session.add(tab_recording)
         db.session.commit()
         return CreateTabRecording(tab_recording=tab_recording)
@@ -28,6 +46,7 @@ class CreateTabRecording(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_tab_recording = CreateTabRecording.Field()
+    create_tab = CreateTab.Field()
 
 
 schema = graphene.Schema(mutation=Mutation)
--- models/models.py
+++ models/models.py
@@ -5,14 +5,14 @@ db = SQLAlchemy()
 
 class Tab(db.Model):
     __tablename__ = 'tabs'
-    id = db.Column(db.Integer, primary_key=True)
-    last_timestamp_active = db.Column(db.DateTime)
+    tab_id = db.Column(db.Integer, primary_key=True)
+    last_timestamp_active = db.Column(db.DateTime, nullable=False)
     window_id = db.Column(db.String(120))
 
 class TabRecording(db.Model):
     __tablename__ = 'tab_recordings'
     id = db.Column(db.Integer, primary_key=True)
-    tab_id = db.Column(db.String(120), db.ForeignKey('tab.id'), nullable=False)
+    tab_id = db.Column(db.Integer, db.ForeignKey('tabs.tab_id'), nullable=False)
     status = db.Column(db.Enum("active", "inactive", name="status_enum", create_type=False, nullable=False))
     in_use = db.Column(db.Boolean)
     timestamp_recorded = db.Column(db.DateTime, nullable=False)
