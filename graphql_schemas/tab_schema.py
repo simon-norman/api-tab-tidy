9eaaed464c62b786b596e7fc2dbdb38dc26760ac
Simon Norman <01secn@gmail.com>
2019-01-30 18:17:25 +0000

    Tab recordings now calculating time since tab last active

--- graphql_schemas/tab_schema.py
+++ graphql_schemas/tab_schema.py
@@ -24,6 +24,11 @@ class CreateTab(graphene.Mutation):
         return CreateTab(tab=tab)
 
 
+def calc_secs_since_tab_active(tab_id, current_timestamp):
+    last_timestamp_active = TabModel.query.get(tab_id).last_timestamp_active
+    time_since_tab_active = current_timestamp - last_timestamp_active
+    return time_since_tab_active.seconds
+
 class TabRecordingObject(SQLAlchemyObjectType):
     class Meta:
         model = TabRecordingModel
@@ -32,13 +37,23 @@ class TabRecordingObject(SQLAlchemyObjectType):
 
 class CreateTabRecording(graphene.Mutation):
     class Arguments:
-        tab_id = graphene.String(required=True)
+        tab_id = graphene.Int(required=True)
         status = graphene.String(required=True)
+        timestamp_recorded = graphene.DateTime(required=True)
 
     tab_recording = graphene.Field(lambda: TabRecordingObject)
 
     def mutate(self, info, tab_id, status, timestamp_recorded):
-        tab_recording = TabRecordingModel(tab_id=tab_id, status=status)
+        if status == 'active':
+            tab = TabModel.query.get(tab_id)
+            tab.last_timestamp_active = timestamp_recorded
+            db.session.commit()
+
+        secs_since_active = calc_secs_since_tab_active(tab_id=tab_id, current_timestamp=timestamp_recorded)
+        tab_recording = TabRecordingModel(tab_id=tab_id,
+                                          status=status,
+                                          secs_since_active=secs_since_active,
+                                          timestamp_recorded=timestamp_recorded)
         db.session.add(tab_recording)
         db.session.commit()
         return CreateTabRecording(tab_recording=tab_recording)
