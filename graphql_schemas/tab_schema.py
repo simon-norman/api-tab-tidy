5aafb09fb0b04c6f4a8e813903975aec02f9b3a1
Simon Norman <01secn@gmail.com>
2019-01-30 19:37:42 +0000

    Changed approach to only store tab open time, close time, and last active time, as this is only data needed. Update Tab mutation added to enable tabs to be updated and closed

--- graphql_schemas/tab_schema.py
+++ graphql_schemas/tab_schema.py
@@ -1,6 +1,6 @@
 import graphene
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models.models import TabRecording as TabRecordingModel, Tab as TabModel
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from models.models import Tab as TabModel
 from models.models import db
 
 
@@ -13,55 +13,37 @@ class TabObject(SQLAlchemyObjectType):
 class CreateTab(graphene.Mutation):
     class Arguments:
         tab_id = graphene.Int(required=True)
-        timestamp_created = graphene.DateTime(required=True)
+        created_timestamp = graphene.DateTime(required=True)
+
 
     tab = graphene.Field(lambda: TabObject)
 
-    def mutate(self, info, tab_id, timestamp_created):
-        tab = TabModel(tab_id=tab_id, last_timestamp_active=timestamp_created)
+    def mutate(self, info, tab_id, created_timestamp):
+        tab = TabModel(tab_id=tab_id, created_timestamp=created_timestamp, last_active_timestamp=created_timestamp)
         db.session.add(tab)
         db.session.commit()
         return CreateTab(tab=tab)
 
-
-def calc_secs_since_tab_active(tab_id, current_timestamp):
-    last_timestamp_active = TabModel.query.get(tab_id).last_timestamp_active
-    time_since_tab_active = current_timestamp - last_timestamp_active
-    return time_since_tab_active.seconds
-
-class TabRecordingObject(SQLAlchemyObjectType):
-    class Meta:
-        model = TabRecordingModel
-        interfaces = (graphene.relay.Node,)
-
-
-class CreateTabRecording(graphene.Mutation):
+class UpdateTab(graphene.Mutation):
     class Arguments:
         tab_id = graphene.Int(required=True)
-        status = graphene.String(required=True)
-        timestamp_recorded = graphene.DateTime(required=True)
+        last_active_timestamp = graphene.DateTime()
+        closed_timestamp = graphene.DateTime()
 
-    tab_recording = graphene.Field(lambda: TabRecordingObject)
 
-    def mutate(self, info, tab_id, status, timestamp_recorded):
-        if status == 'active':
-            tab = TabModel.query.get(tab_id)
-            tab.last_timestamp_active = timestamp_recorded
-            db.session.commit()
+    tab = graphene.Field(lambda: TabObject)
 
-        secs_since_active = calc_secs_since_tab_active(tab_id=tab_id, current_timestamp=timestamp_recorded)
-        tab_recording = TabRecordingModel(tab_id=tab_id,
-                                          status=status,
-                                          secs_since_active=secs_since_active,
-                                          timestamp_recorded=timestamp_recorded)
-        db.session.add(tab_recording)
+    def mutate(self, info, tab_id, **kwargs):
+        tab = TabModel.query.get(tab_id)
+        tab.closed_timestamp = kwargs.get('closed_timestamp', None)
+        tab.last_active_timestamp = kwargs.get('last_active_timestamp', None)
         db.session.commit()
-        return CreateTabRecording(tab_recording=tab_recording)
+        return UpdateTab(tab=tab)
 
 
 class Mutation(graphene.ObjectType):
-    create_tab_recording = CreateTabRecording.Field()
     create_tab = CreateTab.Field()
+    update_tab = UpdateTab.Field()
 
 
 schema = graphene.Schema(mutation=Mutation)
--- models/models.py
+++ models/models.py
@@ -6,17 +6,10 @@ db = SQLAlchemy()
 class Tab(db.Model):
     __tablename__ = 'tabs'
     tab_id = db.Column(db.Integer, primary_key=True)
-    last_timestamp_active = db.Column(db.DateTime, nullable=False)
+    created_timestamp = db.Column(db.DateTime, nullable=False)
+    last_active_timestamp = db.Column(db.DateTime)
+    closed_timestamp = db.Column(db.DateTime)
     window_id = db.Column(db.String(120))
-
-class TabRecording(db.Model):
-    __tablename__ = 'tab_recordings'
-    id = db.Column(db.Integer, primary_key=True)
-    tab_id = db.Column(db.Integer, db.ForeignKey('tabs.tab_id'), nullable=False)
-    status = db.Column(db.Enum("active", "inactive", name="status_enum", create_type=False, nullable=False))
-    in_use = db.Column(db.Boolean)
-    timestamp_recorded = db.Column(db.DateTime, nullable=False)
-    secs_since_active = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Tab %r>' % self.id
