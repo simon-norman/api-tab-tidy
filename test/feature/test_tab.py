537361a31c3b0fa07a95d9366170814b285c29a9
Simon <01secn@gmail.com>
2019-02-21 12:36:45 +0000

    Added pytest test fixture to set up app for feature testing

---
+++ test/feature/test_tab.py
@@ -0,0 +1,21 @@
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def test_client():
+    app = create_app()
+    app.config['TESTING'] = True
+    test_client = app.test_client()
+
+    context = app.app_context()
+    context.push()
+
+    yield test_client
+
+    context.pop()
+
+
+def test_smoke_api(test_client):
+    response = test_client.get('/graphql')
+    print(response)
